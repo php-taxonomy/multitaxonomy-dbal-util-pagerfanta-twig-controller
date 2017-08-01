0e1e196211a6bc7c9d9fef7a28ecb9ed34b8e7ca
Nardberjean <Nardberjean@users.noreply.github.com>
2017-08-01 12:18:19 -0400

    Update MultiTaxonomyController.php

--- MultiTaxonomyController.php
+++ MultiTaxonomyController.php
@@ -69,6 +69,9 @@ class MultiTaxonomyController // extends FrameworkAbstractController
         // if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
         if (!$AuthorizationChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
             // This test may be useless as long as UserInterface is a required argument of the controller indexAction.
+            // http://symfony.com/doc/current/security.html#always-check-if-the-user-is-logged-in
+            // http://symfony.com/doc/current/security.html#add-code-to-deny-access
+            // with this or http://symfony.com/doc/current/security.html#securing-url-patterns-access-control
             throw new AccessDeniedException();
         }
         //^ http://symfony.com/doc/current/security.html#checking-to-see-if-a-user-is-logged-in-is-authenticated-fully
